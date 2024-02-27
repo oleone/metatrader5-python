3cc5e95b6927d8c7de454fef3d2550392dd02ec2
Bruno Leone - Acetime Solucoes <bruno.correa@teros.com.br>
2024-02-27 18:34:34 -0300

    Change

--- account_info.py
+++ account_info.py
@@ -4,20 +4,23 @@ import os
 
 def init_meta_trader_4(login, password, server):
     if not mt5.initialize():
-        print("MetaTrader5 package is not initialized!\n")
+        print("MetaTrader5 package is not started!\n")
         mt5.shurtdown()
     else:
         print("MetaTrader5 started!\n")
 
     timeout = 500
 
-    if mt5.login(login=login, password=password, server=server, timeout=timeout):
+    if mt5.login(login=int(login), password=password, server=server, timeout=timeout):
         print("MetaTrader logged!\n","Server: ",server," Account: ",login)
         print(mt5.account_info())
         return mt5
+    else:
+        dispatch()
+        raise ValueError('MetaTrader is not logged!')
 
 def dispatch():
-    mt5.shurtdown()
+    mt5.shutdown()
 
 def main():
     load_dotenv()
--- orders.py
+++ orders.py
@@ -1,12 +1,28 @@
 import account_info
 from dotenv import load_dotenv
 import os
+from datetime import datetime
 
-load_dotenv()
+def main():
+    load_dotenv()
+    
+    LOGIN = os.getenv('LOGIN')
+    PASSWORD = os.getenv('PASSWORD')
+    SERVER = os.getenv('SERVER')
 
-LOGIN = os.getenv('LOGIN')
-PASSWORD = os.getenv('PASSWORD')
-SERVER = os.getenv('SERVER')
+    account_info.dispatch()
 
-mt5 = account_info.init_meta_trader_4(LOGIN, PASSWORD, SERVER)
-print(mt5.orders_total())
+    from_date=datetime(2019,1,1)
+    to_date=datetime.now()
+
+    mt5 = account_info.init_meta_trader_4(LOGIN, PASSWORD, SERVER)
+
+    print(mt5.positions_get())
+
+    # print(account_info.history_orders_total(date_from = from_date, date_to = to_date))
+
+    # print("\n", mt5.history_orders_get(date_from=from_date, date_to=to_date, group="P*"))
+
+
+if __name__ == "__main__":
+    main()
