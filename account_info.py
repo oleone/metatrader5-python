854783ae5867670d86efa7681ba78c38bc2a5bbb
Bruno Leone - Acetime Solucoes <bruno.correa@teros.com.br>
2024-02-27 15:06:32 -0300

    Enable environments

--- account_info.py
+++ account_info.py
@@ -1,4 +1,6 @@
 import MetaTrader5 as mt5
+from dotenv import load_dotenv
+import os
 
 def init_meta_trader_4(login, password, server):
     if not mt5.initialize():
@@ -18,7 +20,17 @@ def dispatch():
     mt5.shurtdown()
 
 def main():
-    init_meta_trader_4(3166686, "lp.12!@.LP", "XPMT5-PRD")
+    load_dotenv()
+
+    LOGIN = os.getenv('LOGIN')
+    PASSWORD = os.getenv('PASSWORD')
+    SERVER = os.getenv('SERVER')
+
+    print(LOGIN)
+    print(PASSWORD)
+    print(SERVER)
+
+    init_meta_trader_4(LOGIN, PASSWORD, SERVER)
 
 if __name__ == "__main__":
     main()
